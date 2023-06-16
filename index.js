172c57cf1134779a0714d1486daef3e29651177b
Miguel Ramalho <mamramalho@Miguels-MacBook.local>
2023-06-16 06:29:51 +0200

    cors enabling access to backend

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ connection();
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use("/user", users);
 app.use("/auth", auth);
 app.use("/event", events);
